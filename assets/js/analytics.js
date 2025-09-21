b61cef3a8513e8d94330bf760ba92bee37998ebd
Charbel Azar <charbel.azar01@lau.edu>
2025-09-21 11:30:17 +0300

    analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,7 +6,7 @@
     
     // Google Analytics Configuration
     const GA_TRACKING_ID = 'G-H2LEQ4605J';
-    const GA_TRACKING_ID_2 = 'G-8WD87GZ5FX';
+
     
     // Meta Pixel Configuration  
     const FB_PIXEL_ID = '1262147982127732';
